6a8e0f0957cfcd111c1b8e3213af92c82fc2143a
mdaftabuddin56 <114322657+mdaftabuddin56@users.noreply.github.com>
2023-07-26 10:49:13 +0600

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+$(function (){
+   $('.banner-slider').slick({
+      autoplay: true,
+      prevArrow:'<i class="fa-solid fa-arrow-left banner-silder-arrow"></i>',
+      nextArrow: '<i class="fa-solid fa-arrow-right banner-silder-arrow"></i>',
+   })
+   $('.about-slider') .slick();
+})
+$(".silder").slick({
+
+})
